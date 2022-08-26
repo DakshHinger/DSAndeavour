d6bcabbb44a12abd8c2ab7e40a5aaaa9695e32c1
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2022-08-26 23:43:56 +0530

    Contest 806

---
+++ CodeForces/806/1.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+
+const int mod = 1000000007;
+
+void solve() {
+  string su;
+  cin >> su;
+  transform(su.begin(), su.end(), su.begin(), ::toupper);
+  if (su == "YES") {
+    cout << "YES" << endl;
+  } else
+    cout << "NO" << endl;
+}
+
+int32_t main() {
+  fastio();
+  int testcase = 1;
+  cin >> testcase;
+  while (testcase--) {
+    solve();
+  }
+  cout << endl;
+  return 0;
+}
