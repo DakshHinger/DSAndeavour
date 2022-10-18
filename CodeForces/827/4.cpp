865da0538e38e0d18cd3daf8e0afcc5afe59b4c9
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2022-10-19 00:21:16 +0530

    contest 827

---
+++ CodeForces/827/4.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+
+const int mod = 1000000007;
+
+void solve() {
+//   int n;
+//   cin >> n;
+//   vector<int> v(n);
+//   for (int i = 0; i < n; i++) {
+//     cin >> v[i];
+//   }
+//   int ans = -1;
+//   for (int i = 0; i < n; i++) {
+//     for (int j = i; j < n; j++) {
+//       if (__gcd(v[i], v[j]) == 1) {
+//         ans = max(ans, i + j + 2);
+//       }
+//     }
+//   }
+  cout << ans << endl;
+}
+
+int32_t main() {
+  fastio();
+  int testcase = 1;
+  cin >> testcase;
+  while (testcase--) {
+    solve();
+  }
+  cout << endl;
+  return 0;
+}
