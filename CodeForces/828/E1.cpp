ea6af9189e81108e3ce750b78b29465b876f704d
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2022-12-02 23:48:02 +0530

    contest 828

---
+++ CodeForces/828/E1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+
+const int mod = 1000000007;
+
+void solve() {
+  int n;
+  cin >> n;
+}
+
+int32_t main() {
+  fastio();
+  int testcase = 1;
+  // cin >> testcase;
+  while (testcase--) {
+    solve();
+  }
+  cout << endl;
+  return 0;
+}
