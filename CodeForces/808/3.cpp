462c3b886ab9d53b8e29b7a2705e2b3b644a3986
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2022-12-04 16:35:35 +0530

    contest 808

--- CodeForces/808/3.cpp
+++ CodeForces/808/3.cpp
@@ -43,43 +43,17 @@ string maxContest(vector<int> arr, int ind, int iq, string str) {
     return notTake;
   }
 }
-#include <bits/stdc++.h>
-using namespace std;
-
-#define int long long
-#define endl '\n'
-#define fastio()                                                               \
-  ios_base::sync_with_stdio(false);                                            \
-  cin.tie(NULL);                                                               \
-  cout.tie(NULL)
-
-const int mod = 1000000007;
 
 void solve() {
-  int n, l, r;
-  cin >> n >> l >> r;
-  int x, y, e = 0;
-  vector<int> a;
+  int n, q;
+  cin >> n >> q;
+  vector<int> arr(n);
   for (int i = 0; i < n; i++) {
-
-    x = l / (i + 1);
-    y = r / (i + 1);
-    if (x == y && l % (i + 1) != 0) {
-      e = 1;
-      break;
-    } else if (x == y) {
-      a.push_back(l);
-    } else
-      a.push_back(y * (i + 1));
+    cin >> arr[i];
   }
-  if (e == 0) {
-    cout << "YES" << endl;
-    for (int i = 0; i < n; i++) {
-      cout << a[i] << " ";
-    }
-    cout << endl;
-  } else
-    cout << "NO" << endl;
+  string ans = "";
+  ans = maxContest(arr, 0, q, ans);
+  cout << ans << endl;
 }
 
 int32_t main() {
@@ -93,13 +67,9 @@ int32_t main() {
   return 0;
 }
 
-int32_t main() {
-  fastio();
-  int testcase = 1;
-  cin >> testcase;
-  while (testcase--) {
-    solve();
-  }
-  cout << endl;
-  return 0;
-}
+
+
+
+
+
+
