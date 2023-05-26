ca20d82365cec22ef8295147a6fe4526a884de6e
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2023-05-26 17:31:15 +0530

    Time: 8 ms (82.44%) | Memory: 10.2 MB (5.24%) - LeetSync

---
+++ Leetcode/matrix-block-sum/matrix-block-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int K) {
+        std::vector<std::vector<int>>  ret;
+        int m = mat.size();
+        int n = mat[0].size();
+        std::vector<std::vector<int>> dp(m+1,std::vector<int>(n+1,0));
+        for(int i = 1; i <= mat.size(); i++)
+        {
+            for(int j = 1; j <= mat[0].size(); j++)
+            {
+                dp[i][j] = mat[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
+            }
+        }
+        for(int i = 0; i < mat.size(); i++)
+        {
+            std::vector<int> temp;
+            for(int j = 0; j < mat[0].size(); j++)
+            {
+                int rBegin = std::max(0,i - K);
+                int rEnd = std::min(m-1,i + K);
+                int cBegin = std::max(0,j - K);
+                int cEnd = std::min(n - 1, j+K);
+                int value = dp[rEnd+1][cEnd+1] - dp[rEnd+1][cBegin] - dp[rBegin][cEnd+1] + dp[rBegin][cBegin];
+                temp.push_back(value);
+            }
+            ret.push_back(temp);
+        }
+        return ret;
+    }
+};
