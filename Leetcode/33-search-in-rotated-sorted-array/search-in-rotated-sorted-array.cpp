edab34d2d737f8e2b675ae17eadfb45f4fdeef60
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2023-08-13 18:47:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (18.33%) - LeetSync

--- Leetcode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ Leetcode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // if(nums=={1,3}) return 0;
         if(nums.size()==1 && nums[0]==target) return 0;
         int n = nums.size();
         int left = 0, right = n - 1;
@@ -23,7 +22,7 @@ public:
         }
         
         // Binary search over elements on the pivot element's right
-        answer = lower_bound(nums.begin()+ left, nums.end(), target)-nums.begin();
+        answer = lower_bound(nums.begin()+ left, nums.begin()+n-1, target)-nums.begin();
         if (answer!=n && nums[answer]==target) {
             return answer;
         }
