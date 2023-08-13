c9b4c3e6de3e982b89b8f18c0ec7d5a4457ff5a0
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2023-08-13 18:44:10 +0530

    Time: 9 ms (6.98%) | Memory: 11.1 MB (18.33%) - LeetSync

--- Leetcode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ Leetcode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        // if(nums=={1,3}) return 0;
+        if(nums.size()==1 && nums[0]==target) return 0;
         int n = nums.size();
-        int left = 0;
-        int right = n - 1;
-
+        int left = 0, right = n - 1;
+        
+        // Find the index of the pivot element (the smallest element)
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[n - 1]) {
+                left = mid + 1;
             } else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+                right = mid - 1;
             }
         }
-
+        cout<<left<<" ";
+        // Binary search over elements on the pivot element's left
+        int answer = lower_bound(nums.begin(), nums.begin() + left - 1, target) - nums.begin();
+        if (answer!=n && nums[answer]==target) {
+            return answer;
+        }
+        
+        // Binary search over elements on the pivot element's right
+        answer = lower_bound(nums.begin()+ left, nums.end(), target)-nums.begin();
+        if (answer!=n && nums[answer]==target) {
+            return answer;
+        }
         return -1;
     }
 };
